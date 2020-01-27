997cd05896cd79495ed6accf47e7c9c1e9426f7c
Jinkyu Han <hjk1019@gmail.com>
2020-01-27 22:24:20 +0900

    INDEX 페이지 스크립트 위치 변경

--- src/main/resources/static/scripts/index.js
+++ src/main/resources/static/scripts/index.js
@@ -3,6 +3,22 @@
 let app = new Vue({
     el: '#app',
     data: {
-        title: 'Hello!'
+        products: []
+    },
+    methods: {
+        getProdutcs: function () {
+            let that = this;
+            axios.get('/products')
+                .then(res => {
+                    that.products = res.data;
+                })
+                .catch(err => {
+                    console.log(err);
+                    alert('error occurred!');
+                });
+        }
+    },
+    mounted: function () {
+        this.getProdutcs();
     }
 });
